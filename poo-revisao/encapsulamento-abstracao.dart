26002921537825b79a0f6b16095cca9c4968f9ab
edugmenes <edugmenes1@hotmail.com>
2024-04-11 13:26:35 -0300

    Details: - Adição dos arquivos de revisão de POO * "heranca-polimorfismo" * "abstracao-encapsulamento"

---
+++ poo-revisao/encapsulamento-abstracao.dart
@@ -0,0 +1,119 @@
+//POO -> 4 PILARES FUNDAMENTAIS
+/*
+1 - ABSTRAÇÃO
+2 - ENCAPSULAMENTO
+3 - HERANÇA
+4 - POLIMORFISMO
+EXEMPLO FATURA SEM PARAR: 
+
+- FATURA
+  - ATRIBUTOS:
+    - NDOC
+    - DTVENC
+    - VALOR
+    - CLIENTE
+  - MÉTODOS:
+    - SEGUNDA_VIA()
+    - GERAR()
+    - PAGAR()
+    - VERIFICAR_INADIMPLENTES()
+    - (create, update, read, delete)
+- CLIENTE
+  - ATRIBUTOS:
+    - NOME
+    - CPF
+    - EMAIL
+  - MÉTODOS:
+    - INATIVAR()
+    - ATIVAR()
+    - SUSPENDER()
+    - (create, update, read, delete)
+*/
+void main() {
+  try {
+    Cliente c1 = new Cliente();
+    //c1.nome = "Raphael";
+    c1.setNome("");
+    c1.email = "Raphael";
+    Fatura f = new Fatura();
+  } on Exception catch (e) {
+    print(e.toString());
+  }
+}
+
+/* 
+ENCAPSULAMENTO -> Atributos da classe devem se tornar privados!
+Em dart utiliza-se '_' para tornar o recurso privado.
+-> Para leitura e escrita deve-se ofertar métodos get/set
+*/
+class Cliente {
+  //ATRIBUTOS
+  String? _nome;
+  String? _cpf;
+  String? _email;
+  //MÉTODOS CONSTRUTORES
+  Cliente() {}
+  Cliente.todos(this._nome, this._cpf, this._email);
+  /* FORMATO CONVENCIONAL DE SE DECLARAR UM CONSTRUTOR
+  Cliente.todos(String nome, String cpf, String email){
+    this.nome = nome;
+    this.cpf = cpf;
+    this.email = email;
+  }
+  EM DART UMA FORMA ANALÓGA É DESCRITA ABAIXO
+  */
+
+  //MÉTODOS (RETORNAM OU NÃO RETORNAM VALOR)
+  void inativar() {}
+  void ativar() {}
+  void suspender() {}
+  //MÉTODOS ENCAPSULAMENTO
+  //GETTERS/SETTERS
+  //GET -> MÉTODO DE LEITURA
+
+  //ESCRITA DOS MÉTODOS DE MODO CONVENCIONAL
+  String? getNome() {
+    return this._nome;
+  }
+
+  void setNome(String nome) {
+    if (!nome.isEmpty) this._nome = nome;
+  }
+
+  String? getCPF() {
+    return this._cpf;
+  }
+
+  /*
+  EXEMPLO DE USO DE ARROW FUNCTION =>
+  String? get nome => this._nome;
+  
+  EXEMPLO DE USO MÉTODO GET/SET EM DART (RECOMENDADO)
+  -> DESTA FORMA É NECESSÁRIO QUE O MÉTODO GET TENHA O MESMO 
+  NOME DO ATRIBUTO, PORÉ, DE MANEIRA PÚBLICA.
+  */
+  String? get email {
+    return this._email;
+  }
+
+  set email(String? email) {
+    /* EXEMPLO OPERAÇÃO TERNÁRIA DA CONDIÇÃO DE VALIDAÇÃO DO E-MAIL
+    (!email!.contains('@')) ? 
+      throw new Exception("E-mail inválido!") : 
+      this._email = email;
+    */
+    if (!email!.contains('@'))
+      throw new Exception("E-mail inválido!");
+    else
+      this._email = email;
+  }
+}
+
+class Fatura {
+  int? _ndoc;
+  DateTime? _dtVenc;
+  double? _valor;
+  Cliente? _cliente;
+
+  Fatura() {}
+}
---
+++ poo-revisao/heranca-polimorfismo.dart
@@ -0,0 +1,67 @@
+import 'dart:io';
+
+void main() {
+  Funcionario f = new Funcionario(
+      "Raphael", "raphael@uscs", new Endereco("Rua xxx"), 99999999);
+  Pessoa p = new Pessoa.all("Pessoa", "@", new Endereco("Rua yyyy"));
+  print(f.nome);
+  //f.Consultar();
+  //p.Consultar();
+  //f.Incluir();
+  //print("------- PESSOA -------");
+  //p.Incluir();
+  Endereco e = new Endereco("Rua");
+  print(e.toString());
+}
+
+class Pessoa {
+  String? nome;
+  String? email;
+  Endereco? endereco;
+
+  Pessoa.all(this.nome, this.email, this.endereco);
+
+  void Incluir() {
+    print("Iniciando conexão com o DB...");
+    sleep(new Duration(seconds: 1));
+    print("Incluindo dados na tabela...");
+    sleep(new Duration(seconds: 1));
+    print("Dados de pessoa armazenados com sucesso!");
+  }
+
+  void Alterar() {}
+  void Excluir() {}
+  void Consultar() {
+    print('${nome} - ${email} - ${endereco?.logradouro}');
+  }
+}
+
+class Funcionario extends Pessoa {
+  double salario;
+
+  Funcionario(String? nome, String? email, Endereco? endereco, this.salario)
+      : super.all(nome, email, endereco);
+//EXEMPLO DE REESCRITA TOTAL DO MÉTODO
+  @override
+  void Consultar() {
+    print('${email} - ${nome} - ${this.salario}');
+  }
+
+  //REESCRITA PARCIAL DO MÉTODO/COMPLEMENTAR DO MÉTODO
+  @override
+  void Incluir() {
+    super.Incluir();
+    print("Incluindo salário na tabela funcionário...");
+    print("Dados de funcionário armazenados com sucesso!");
+  }
+}
+
+class Endereco {
+  String logradouro;
+  Endereco(this.logradouro);
+
+  @override
+  String toString() {
+    return "Endereco: ${logradouro}";
+  }
+}
